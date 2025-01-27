c2fde0667191dd410c2c1c6001a5b49c85ca7fc4
노수민 <nosumin29@gmail.com>
2025-01-27 15:17:51 +0900

    feat: 트랜스파일러 적용하여 JSX사용하기

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello, World!");
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(/*#__PURE__*/React.createElement(App, null));
